ff996d247c56ebc005dbed3027adbe6d8e33d769
Cralcas <carlosbergq@gmail.com>
2024-02-22 22:04:10 +0100

    Variables for the HTML elements

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,13 @@
 import "../sass/style.scss";
 import {taskModal} from "./modal";
 
+const titleInput = document.getElementById("titleInput") as HTMLElement;
+const dateInput = document.getElementById("dateInput") as HTMLElement;
+const descriptionInput = document.getElementById(
+  "descriptionInput"
+) as HTMLElement;
+const addTaskButton = document.getElementById("addTaskButton") as HTMLElement;
+
+const todoData: Array<string> = [];
+
 taskModal();
